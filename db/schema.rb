13edb700645ad4a473044b05091e3ecf2d3f2876
github username <github email address>
2019-04-07 22:47:54 -0400

    schema update with work experiences migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_223925) do
+ActiveRecord::Schema.define(version: 2019_04_08_001811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2019_04_07_223925) do
     t.string "full_name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "work_experiences", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "business_name"
+    t.string "description"
+    t.integer "year_began"
+    t.integer "year_ended"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
